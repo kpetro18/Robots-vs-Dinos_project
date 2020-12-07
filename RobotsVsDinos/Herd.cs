e5309f1018c486ffb7b00dc28bbbbbb3ef10d9cf
kevin petroviak <kpetro18@gmail.com>
2020-12-07 09:32:02 -0600

    created required classes

---
+++ RobotsVsDinos/Herd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotsVsDinos
+{
+    class Herd
+    {
+        //member variables (HAS A)
+
+        //constructor (SPAWNER)
+
+        //member methods (CAN DO)
+
+
+    }
+}
